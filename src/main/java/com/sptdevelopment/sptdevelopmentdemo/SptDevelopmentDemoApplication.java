abb3342b4b4a59602b22ae0ffed2c1535d0b99eb
Simon Taylor <simon.taylor@spt-development.com>
2021-09-01 20:50:41 +0100

    Creation of demo project, to demonstrate SPT opensource projects

---
+++ src/main/java/com/sptdevelopment/sptdevelopmentdemo/SptDevelopmentDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sptdevelopment.sptdevelopmentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptDevelopmentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptDevelopmentDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sptdevelopment/sptdevelopmentdemo/SptDevelopmentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sptdevelopment.sptdevelopmentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SptDevelopmentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
