936d31f21db89a42d94fbf1e86f4dc8c46874a9b
Simon Taylor <simon.taylor@spt-development.com>
2021-09-03 18:13:20 +0100

    Added in spring security

--- src/main/resources/db/migration/V0001__initial_setup.sql
+++ src/main/resources/db/migration/V0001__initial_setup.sql
@@ -7,3 +7,8 @@ CREATE TABLE demo.book (
     author  TEXT NOT NULL,
     rrp     INT NOT NULL
 );
+
+/* INSERT some books as this is for demo purposes */
+INSERT INTO demo.book (title, blurb, author, rrp) VALUES ('1984', 'Hidden away in the Record Department of the sprawling Ministry of Truth, Winston Smith skilfully rewrites the past to suit the needs to the Party.', 'George Orwell', 899);
+INSERT INTO demo.book (title, blurb, author, rrp) VALUES ('Fahrenheit 451', 'Guy Montag is a fireman. His job is to destroy the most illegal of commodities, the source of all discord an unhappiness: the printed book.', 'Ray Bradbury', 899);
+INSERT INTO demo.book (title, blurb, author, rrp) VALUES ('The Hitchhikers Guide to the Galaxy', 'One Thursday lunchtime the Earth gets unexpectedly demolished to make way for a new hyperspace bypass.', 'Douglas Adams', 699);
