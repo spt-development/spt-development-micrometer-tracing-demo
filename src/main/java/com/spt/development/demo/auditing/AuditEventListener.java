6919e268a286f66a1121ccd13ee83ca8a2478c27
Simon Taylor <simon.taylor@spt-development.com>
2021-09-04 15:50:48 +0100

    Added auditing of successful/failed login attempts

---
+++ src/main/java/com/spt/development/demo/auditing/AuditEventListener.java
@@ -0,0 +1,75 @@
+package com.spt.development.demo.auditing;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.spt.development.audit.spring.AuditEventWriter;
+import com.spt.development.cid.CorrelationId;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.audit.AuditEvent;
+import org.springframework.boot.actuate.audit.listener.AbstractAuditListener;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Set;
+
+import static com.spt.development.audit.spring.util.HttpRequestUtils.getClientIpAddress;
+import static com.spt.development.demo.util.Constants.Auditing;
+import static org.springframework.boot.actuate.security.AuthenticationAuditListener.AUTHENTICATION_FAILURE;
+import static org.springframework.boot.actuate.security.AuthenticationAuditListener.AUTHENTICATION_SUCCESS;
+
+/**
+ * This listener is an example of how the spt-development-audit-spring auditing framework can be combined with actuators
+ * audit events to audit successful and failed login attempts.
+ */
+@Slf4j
+@Component
+public class AuditEventListener extends AbstractAuditListener {
+    private static final Set<String> AUTHENTICATION_EVENTS = Set.of(AUTHENTICATION_SUCCESS, AUTHENTICATION_FAILURE);
+    private static final Gson GSON = new GsonBuilder().create();
+
+    private final String appName;
+    private final BuildProperties buildProperties;
+    private final AuditEventWriter auditEventWriter;
+
+    public AuditEventListener(
+            @Value("${spring.application.name}")
+            final String appName,
+            final BuildProperties buildProperties,
+            final AuditEventWriter auditEventWriter) {
+
+        this.appName = appName;
+        this.buildProperties = buildProperties;
+        this.auditEventWriter = auditEventWriter;
+    }
+
+    @Override
+    protected void onAuditEvent(AuditEvent event) {
+        LOG.debug("[{}] On audit event: {}", CorrelationId.get(), event);
+
+        try {
+            if (AUTHENTICATION_EVENTS.contains(event.getType())) {
+                auditEventWriter.write(
+                        com.spt.development.audit.spring.AuditEvent.builder()
+                                .type(Auditing.Type.SECURITY)
+                                .subType(event.getType())
+                                .correlationId(CorrelationId.get())
+                                .details(GSON.toJson(event.getData()))
+                                .username(event.getPrincipal())
+                                .originatingIP(getClientIpAddress())
+                                .serviceId(appName)
+                                .serviceVersion(buildProperties.getVersion())
+                                .serverHostName(InetAddress.getLocalHost().getHostName())
+                                .created(OffsetDateTime.now(ZoneOffset.UTC))
+                                .build()
+                );
+            }
+        }
+        catch (Throwable t) {
+            LOG.error("[{}] Failed to send audit event: {}", CorrelationId.get(), event, t);
+        }
+    }
+}
---
+++ src/main/java/com/spt/development/demo/config/AuditConfig.java
@@ -0,0 +1,22 @@
+package com.spt.development.demo.config;
+
+import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuditConfig {
+
+    /**
+     * This bean is required to switch actuator security auditing on - see
+     * {@link org.springframework.boot.actuate.autoconfigure.audit.AuditAutoConfiguration}
+     *
+     * @return an instance of {@link InMemoryAuditEventRepository}.
+     */
+    @Bean
+    public AuditEventRepository auditEventRepository() {
+        // This bean is required to switch actuator security auditing on - see {@link org.springframework.boot.actuate.autoconfigure.audit.AuditAutoConfiguration}
+        return new InMemoryAuditEventRepository();
+    }
+}
